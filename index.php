bff8bb6e739b2e28ea72bf8791e721e598e149e4
josefdezmanzano <josefernandezmanzano@hotmail.com>
2019-02-21 14:27:24 +0100

    falta terminar

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -10,8 +11,26 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <?php 
+        if(isset($_POST["btn_env"])){
+        $nombre=$_POST["nombre"];
+        $password=$_POST["password"];
+            echo $nombre;
+            echo '<br>'.$password;
+        }else{
+        
+        ?>
+        <form method="POST" action="" enctype="multipart/form-data">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre">
+            <label for="password">Password:</label>
+            <input type="text" name="password">
+
+            <input type="hidden" name="csrf" value="<?php echo $_SESSION["token"]; ?>">
+            <input type="submit" name="btn_env">
+        </form>
         <?php
-       echo "holi";
+        }
         ?>
     </body>
 </html>
