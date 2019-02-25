4d39f2f8e46e23e3284fd379a5191fdae2fe67cd
seillo <seillo@equipo2daw12>
2019-02-25 09:29:26 +0100

    Hacemos un merge

--- veneno.php
+++ veneno.php
@@ -2,5 +2,8 @@
 <body>
 <p>Esta página es veneno!!</p>
 <img src="process.php?action=logout" style="display:none;">
+
+Esta es la modificacion que hemos echo en la rama de modificacionesRaras
+
 </body>
 </html>
