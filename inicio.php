25ff72cfd2e8cd3a5ff56ed502e8f3a6e367a62b
seillo <josefernandezmanzano@hotmail.com>
2019-03-05 20:34:41 +0100

    finished

--- inicio.php
+++ inicio.php
@@ -2,6 +2,17 @@
 session_start();
 //$length = 32;
 //$_SESSION['token'] = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $length);
+if(isset($_COOKIE['ultimaVez'])){
+    setcookie("ultimaVez",$_COOKIE['ultimaVez']+1,time()+365*60*60);
+    setcookie("horaUltimaVez",date("Y-m-d H:i:s"),time()+365*60*60);
+    
+
+}else{
+    setcookie("ultimaVez",1,time()+365*60*60);
+    setcookie("horaUltimaVez",date("Y-m-d H:i:s"),time()+365*60*60);
+
+}
+
 $token=$_SESSION['token'];
 
 ?>
@@ -17,6 +28,18 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+    <?php 
+    if($_COOKIE['ultimaVez']==0){
+echo "<h4>Bienvenido, Esta es tu primera visita a nuestra pagina!!!</h4>";
+    }
+    if($_COOKIE['ultimaVez']>0){
+        echo "<h4>Bienvenido, Esta es tu visita numero ". $_COOKIE['ultimaVez'] ." a nuestra pagina!!!</h4>";
+        echo "<h3>La ultima vez que visitaste la pagina fue el ". $_COOKIE['horaUltimaVez']." </h3>";
+
+    }
+            
+    
+    ?>
         <form method="POST" action="validar.php?action=logout">  
         <input type="hidden" name="tokenform" value=<?php echo "$token"; ?>>
             <input type="submit" value="Cerrar Sesion"/>
